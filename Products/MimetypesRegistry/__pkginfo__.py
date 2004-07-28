a20c511ef2ac03cf02a2dea0ce307c1f9df570b5
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-07-28 22:58:09 +0000

    Preparing release 1.3.0-2
    svn path=/MimetypesRegistry/trunk/; revision=2871

--- Products/MimetypesRegistry/__pkginfo__.py
+++ Products/MimetypesRegistry/__pkginfo__.py
@@ -1,8 +1,8 @@
 
 modname = 'MimetypesRegistry'
-version = '1.3.0-1'
-numversion = (1, 3, 0, 1)
-at_versions = ('1.3.0-beta3', )
+version = '1.3.0-2'
+numversion = (1, 3, 0, 2)
+at_versions = ('1.3.0-beta3', '1.3.0-beta4', )
 
 license = 'BSD like'
 license_text = open('LICENSE.txt').read()
