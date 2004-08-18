2025b0ca52da7ee5088cf64679f60bf67dd2d407
Christian 'Tiran' Heimes <lists@cheimes.de>
2004-08-18 10:23:40 +0000

    Preparing Archetypes 1.3.0-rc1 release (tagged as at_release-1_3_0-rc1)
    svn path=/MimetypesRegistry/trunk/; revision=2880

--- Products/MimetypesRegistry/__init__.py
+++ Products/MimetypesRegistry/__init__.py
@@ -1,4 +1,4 @@
-__version__ = (1, 3, 0, 3)
+__version__ = (1, 3, 1, 1)
 
 import common 
 
--- Products/MimetypesRegistry/__pkginfo__.py
+++ Products/MimetypesRegistry/__pkginfo__.py
@@ -1,8 +1,8 @@
 
 modname = 'MimetypesRegistry'
-version = '1.3.0-3'
-numversion = (1, 3, 0, 3)
-at_versions = ('1.3.0-beta3', '1.3.0-beta4', '1.3.0-beta5', )
+version = '1.3.1-1'
+numversion = (1, 3, 1, 1)
+at_versions = ('1.3.0-beta3', '1.3.0-beta4', '1.3.0-beta5', '1.3.0-rc1', )
 
 license = 'BSD like'
 license_text = open('LICENSE.txt').read()
